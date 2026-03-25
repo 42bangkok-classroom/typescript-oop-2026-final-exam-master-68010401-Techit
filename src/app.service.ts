e8dde39667310c4f58fd3610d6dd1d266b385800
68010401-Techit <68010401@kmitl.ac.th>
2026-03-25 11:49:39 +0700

    4

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { version } from 'os';
-import { ApiResponse } from './interfaces/response.interface';
 
 @Injectable()
 export class AppService {
