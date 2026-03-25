f6e3fbdb713477a25b7bb18ff7ab6a696bc5468d
68010401-Techit <68010401@kmitl.ac.th>
2026-03-25 11:26:44 +0700

    2

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { version } from 'os';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello NestJS';
+  getHello(){
+  const response = {
+    success : true,
+    data : {
+      service : "purchase-api",
+      version : "1.0.0"
+    },
+    message : "Hello NestJS"
   }
-}
+  return response;
+  }
+  }
+
